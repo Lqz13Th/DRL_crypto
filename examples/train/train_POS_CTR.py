37eb88a6adca84e08ff4d15229815ab9bfcb4ebe
Lqz13Th <lqz13th@outlook.com>
2024-11-05 18:16:37 +0800

    add pos control env

---
+++ examples/train/train_POS_CTR.py
@@ -0,0 +1,143 @@
+import polars as pl
+from stable_baselines3.common.env_util import make_vec_env
+from stable_baselines3 import PPO
+
+from engine.evaluation.match_module import MatchEngine
+from engine.evaluation.order_module import Order
+
+from ppo_algo.envs.pos_control_env import PositionControlEnv
+from engine.callback import TensorboardCallback
+
+if __name__ == '__main__':
+    df = pl.read_csv("C:/Users/trade/PycharmProjects/DRL_crypto/utils/normalized_data_0.001.csv")
+    print(df)
+
+    token = "FIL-USDT"
+    # n_updates = total_time_steps // (n_steps * n_envs)
+    env = make_vec_env(lambda: PositionControlEnv(df, token), n_envs=8)
+    # tensorboard --logdir=examples/train/pos_ctr_tensorboard
+
+    model = PPO(
+        policy="MultiInputPolicy",
+        env=env,
+        verbose=2,
+        learning_rate=3e-4,
+        n_steps=256,
+        # Number of steps to collect in each environment before updating
+        batch_size=64,  # Batch size used for optimization
+        n_epochs=10,
+        gamma=0.99,
+        clip_range=0.2,
+        clip_range_vf=None,
+        normalize_advantage=True,
+        ent_coef=0.0,
+        vf_coef=0.5,
+        max_grad_norm=0.5,
+        use_sde=True,  # False for discrete actions
+        sde_sample_freq=20,
+        target_kl=None,
+        stats_window_size=100,
+        seed=13,
+        device="auto",
+        tensorboard_log="./pos_ctr_tensorboard/",
+    )
+
+    model.learn(
+        total_timesteps=10**10,
+        callback=TensorboardCallback(target_episodes=10, verbose=1),
+    ).save(
+        path="ppo_pos_ctr",
+    )
+
+    del model, env
+
+    env = make_vec_env(lambda: PositionControlEnv(df, token, mode='evaluation'), n_envs=1)
+    model = PPO.load("ppo_pos_ctr")
+    obs = env.reset()
+    backtest = MatchEngine(debug=True)
+    backtest.token_default(token)
+    a = 0
+    while True:
+        action, _states = model.predict(obs, deterministic=True)
+        obs, rewards, dones, infos = env.step(action)
+
+        price = infos[0]['price']
+        match action[0]:
+            case action if action >= 0.99:
+                order = Order(side=1, price=price, size=500, order_type="market")
+                backtest.check_orders(price=price, token=token, orders=[order])
+                print(
+                    "buy5",
+                )
+
+            case action if 0.99 > action >= 0.8:
+                order = Order(side=1, price=price, size=200, order_type="market")
+                backtest.check_orders(price=price, token=token, orders=[order])
+                print(
+                    "buy4",
+                )
+
+            case action if 0.8 > action >= 0.6:
+                order = Order(side=1, price=price, size=100, order_type="market")
+                backtest.check_orders(price=price, token=token, orders=[order])
+                print(
+                    "buy3",
+                )
+
+            case action if 0.6 > action >= 0.4:
+                order = Order(side=1, price=price, size=50, order_type="market")
+                backtest.check_orders(price=price, token=token, orders=[order])
+                print(
+                    "buy2",
+                )
+
+            case action if 0.4 > action >= 0.2:
+                order = Order(side=1, price=price, size=10, order_type="market")
+                backtest.check_orders(price=price, token=token, orders=[order])
+                print(
+                    "buy1",
+                )
+
+            case action if action <= -0.99:
+                order = Order(side=-1, price=price, size=500, order_type="market")
+                backtest.check_orders(price=price, token=token, orders=[order])
+                print(
+                    "sell5",
+                )
+
+            case action if -0.99 < action <= -0.8:
+                order = Order(side=-1, price=price, size=200, order_type="market")
+                backtest.check_orders(price=price, token=token, orders=[order])
+                print(
+                    "sell4",
+                )
+
+            case action if -0.8 < action <= -0.6:
+                order = Order(side=-1, price=price, size=100, order_type="market")
+                backtest.check_orders(price=price, token=token, orders=[order])
+                print(
+                    "sell3",
+                )
+
+            case action if -0.6 < action <= -0.4:
+                order = Order(side=-1, price=price, size=50, order_type="market")
+                backtest.check_orders(price=price, token=token, orders=[order])
+                print(
+                    "sell2",
+                )
+
+            case action if -0.4 < action <= -0.2:
+                order = Order(side=-1, price=price, size=10, order_type="market")
+                backtest.check_orders(price=price, token=token, orders=[order])
+                print(
+                    "sell1",
+                )
+
+            case _:
+                backtest.check_orders(price=price, token=token)
+
+        if dones[0]:
+            break
+
+    backtest.calculate_max_drawdown()
+    backtest.plot()
---
+++ ppo_algo/envs/pos_control_env.py
@@ -0,0 +1,337 @@
+import numpy as np
+import gymnasium as gym
+import polars as pl
+
+from gymnasium import spaces
+
+from engine.evaluation.order_module import Order
+from engine.evaluation.match_module import MatchEngine
+from utils.normalizer import scaled_sigmoid
+
+
+class PositionControlEnv(gym.Env):
+    def __init__(
+            self,
+            data_frame: pl.DataFrame,
+            single_token: str,
+            mode='train',
+    ):
+        super(PositionControlEnv, self).__init__()
+        self.me = MatchEngine(debug=True)
+        self.me.token_default(single_token)
+
+        self.features_df = data_frame.select([
+            "ts",
+            "price",
+            "scaled_sum_buy_size",
+            "scaled_sum_sell_size",
+            "scaled_timestamp_duration",
+            "scaled_price_pct_change",
+            "scaled_buy_sell_imbalance",
+            "scaled_change_side",
+            "scaled_price_pct_change_sum_10"
+        ]).drop_nulls()
+
+        self.mode = mode
+
+        self.row_idx = 0
+        self.total_idx = self.features_df.shape[0]
+        self.total_train_idx = int(self.total_idx * 0.7)
+
+        self.pnl_rate = 0
+        self.pos_value_rate = 0
+        self.current_pos_rate = 0
+
+        self.token = single_token
+        self.price = self.features_df[0, "price"]
+        self.pos_hold_penalty = 0
+
+        self.action_space = spaces.Box(low=-1, high=1, shape=(1,), dtype=np.float32)
+
+        # multi modal | tensorboard --logdir=examples/train/POS_CTR_tensorboard/
+        self.observation_space = spaces.Dict({
+            "features": spaces.Box(low=0, high=1, shape=(7, ), dtype=np.float32),
+            "account_states": spaces.Box(low=0, high=1, shape=(4,), dtype=np.float32),
+        })
+
+    def reset(self, seed=None, options=None):  # 添加 seed 参数
+        super().reset(seed=seed)
+        # self.row_idx = self.total_train_idx if self.mode == "evaluation" else 0
+        self.row_idx = 0
+        self.pnl_rate = 0
+        self.pos_value_rate = 0
+        self.current_pos_rate = 0
+
+        self.me.evaluation_default()
+        self.me.account_default()
+        self.me.token_default(self.token)
+
+        print(self.features_df)
+        print(self.features_df.row(self.row_idx), type(self.features_df.row(self.row_idx)))
+        print(self.row_idx, self.total_train_idx, self.total_idx)
+        init_obs = {
+            "features": np.array(self.features_df.row(self.row_idx)[2:], dtype=np.float32),
+            "account_states": np.array(
+                [
+                    scaled_sigmoid(self.pnl_rate, -0.1, 0.1),
+                    scaled_sigmoid(self.pos_value_rate, -1, 1),
+                    scaled_sigmoid(self.current_pos_rate, -0.5, 0.5),
+                    scaled_sigmoid(self.me.side[self.token], -1, 1),
+                ],
+                dtype=np.float32,
+            ),
+        }
+
+        return init_obs, {}
+
+    def step(self, action_state):
+        self._take_action(action_state)
+
+        reward = self._calculate_reward(action_state)
+
+        terminated = False
+        if self.mode == "evaluation":
+            if self.row_idx >= self.total_idx:
+                terminated = True
+                print("done", self.me.funds)
+                self.row_idx = 0
+
+        elif self.row_idx >= self.total_train_idx:
+            terminated = True
+            self.row_idx = 0
+
+        elif self.me.funds < self.me.init_funds * 0.5:
+            print("Liquidation!")
+            terminated = True
+            reward = - 0.5
+            self.row_idx = 0
+
+        truncated = False
+
+        self.price = self.features_df.row(self.row_idx)[1]
+        next_obs = self._next_observation()
+        infos = {
+            "price": self.price,
+        }
+
+        # print(next_obs)
+        return next_obs, reward, terminated, truncated, infos
+
+    def _next_observation(self):
+        next_obs = {
+            "features": np.array(self.features_df.row(self.row_idx)[2:], dtype=np.float32),
+            "account_states": np.array(
+                [
+                    scaled_sigmoid(self.pnl_rate, -0.1, 0.1),
+                    scaled_sigmoid(self.pos_value_rate, -1, 1),
+                    scaled_sigmoid(self.current_pos_rate, -0.5, 0.5),
+                    scaled_sigmoid(self.me.side[self.token], -1, 1),
+                ],
+                dtype=np.float32,
+            ),
+        }
+        self.row_idx += 1
+
+        return next_obs
+
+    def _take_action(self, action):
+        print(
+            self.row_idx,
+            action[0],
+            self.me.side[self.token],
+            self.me.funds,
+            self.me.cumulative_pnl,
+            self.me.average_price[self.token],
+            self.price,
+            self.me.position[self.token],
+            self.me.cumulative_pos_value
+        )
+        self.me.update_pos_value()
+
+        match action[0]:
+            case action if action >= 0.99:
+                order = Order(side=1, price=self.price, size=500, order_type="market")
+                self.me.check_orders(price=self.price, token=self.token, orders=[order])
+                print(
+                    self.row_idx,
+                    "buy5",
+                    self.price,
+                    self.me.funds,
+                    self.me.cumulative_pnl,
+                    self.me.average_price[self.token],
+                    self.me.position[self.token],
+                    self.me.cumulative_pos_value
+                )
+
+            case action if 0.99 > action >= 0.8:
+                order = Order(side=1, price=self.price, size=200, order_type="market")
+                self.me.check_orders(price=self.price, token=self.token, orders=[order])
+                print(
+                    self.row_idx,
+                    "buy4",
+                    self.price,
+                    self.me.funds,
+                    self.me.cumulative_pnl,
+                    self.me.average_price[self.token],
+                    self.me.position[self.token],
+                    self.me.cumulative_pos_value
+                )
+
+            case action if 0.8 > action >= 0.6:
+                order = Order(side=1, price=self.price, size=100, order_type="market")
+                self.me.check_orders(price=self.price, token=self.token, orders=[order])
+                print(
+                    self.row_idx,
+                    "buy3",
+                    self.price,
+                    self.me.funds,
+                    self.me.cumulative_pnl,
+                    self.me.average_price[self.token],
+                    self.me.position[self.token],
+                    self.me.cumulative_pos_value
+                )
+
+            case action if 0.6 > action >= 0.4:
+                order = Order(side=1, price=self.price, size=50, order_type="market")
+                self.me.check_orders(price=self.price, token=self.token, orders=[order])
+                print(
+                    self.row_idx,
+                    "buy2",
+                    self.price,
+                    self.me.funds,
+                    self.me.cumulative_pnl,
+                    self.me.average_price[self.token],
+                    self.me.position[self.token],
+                    self.me.cumulative_pos_value
+                )
+
+            case action if 0.4 > action >= 0.2:
+                order = Order(side=1, price=self.price, size=10, order_type="market")
+                self.me.check_orders(price=self.price, token=self.token, orders=[order])
+                print(
+                    self.row_idx,
+                    "buy1",
+                    self.price,
+                    self.me.funds,
+                    self.me.cumulative_pnl,
+                    self.me.average_price[self.token],
+                    self.me.position[self.token],
+                    self.me.cumulative_pos_value
+                )
+
+            case action if action <= -0.99:
+                order = Order(side=-1, price=self.price, size=500, order_type="market")
+                self.me.check_orders(price=self.price, token=self.token, orders=[order])
+                print(
+                    self.row_idx,
+                    "sell5",
+                    self.price,
+                    self.me.funds,
+                    self.me.cumulative_pnl,
+                    self.me.average_price[self.token],
+                    self.me.position[self.token],
+                    self.me.cumulative_pos_value
+                )
+
+            case action if -0.99 < action <= -0.8:
+                order = Order(side=-1, price=self.price, size=200, order_type="market")
+                self.me.check_orders(price=self.price, token=self.token, orders=[order])
+                print(
+                    self.row_idx,
+                    "sell4",
+                    self.price,
+                    self.me.funds,
+                    self.me.cumulative_pnl,
+                    self.me.average_price[self.token],
+                    self.me.position[self.token],
+                    self.me.cumulative_pos_value
+                )
+
+            case action if -0.8 < action <= -0.6:
+                order = Order(side=-1, price=self.price, size=100, order_type="market")
+                self.me.check_orders(price=self.price, token=self.token, orders=[order])
+                print(
+                    self.row_idx,
+                    "sell3",
+                    self.price,
+                    self.me.funds,
+                    self.me.cumulative_pnl,
+                    self.me.average_price[self.token],
+                    self.me.position[self.token],
+                    self.me.cumulative_pos_value
+                )
+
+            case action if -0.6 < action <= -0.4:
+                order = Order(side=-1, price=self.price, size=50, order_type="market")
+                self.me.check_orders(price=self.price, token=self.token, orders=[order])
+                print(
+                    self.row_idx,
+                    "sell2",
+                    self.price,
+                    self.me.funds,
+                    self.me.cumulative_pnl,
+                    self.me.average_price[self.token],
+                    self.me.position[self.token],
+                    self.me.cumulative_pos_value
+                )
+
+            case action if -0.4 < action <= -0.2:
+                order = Order(side=-1, price=self.price, size=10, order_type="market")
+                self.me.check_orders(price=self.price, token=self.token, orders=[order])
+                print(
+                    self.row_idx,
+                    "sell1",
+                    self.price,
+                    self.me.funds,
+                    self.me.cumulative_pnl,
+                    self.me.average_price[self.token],
+                    self.me.position[self.token],
+                    self.me.cumulative_pos_value
+                )
+
+            case _:
+                self.me.check_orders(price=self.price, token=self.token)
+
+    def _calculate_reward(self, action):
+        pnl = self.me.cumulative_pnl
+        pos_value = self.me.cumulative_pos_value
+        avg_pos_price = self.me.average_price[self.token]
+        current_price = self.price
+
+        pnl_rate = pnl / self.me.init_funds
+        pos_value_rate = pos_value / self.me.funds
+        abs_pos_value_rate = abs(pos_value_rate)
+
+        current_pos_rate = 0
+
+        match self.me.side[self.token]:
+            case 1:
+                current_pos_rate = (current_price - avg_pos_price) / avg_pos_price
+                self.pos_hold_penalty += 1
+
+            case -1:
+                current_pos_rate = (avg_pos_price - current_price) / avg_pos_price
+                self.pos_hold_penalty += 1
+
+            case _:
+                self.pos_hold_penalty = 0
+                pass
+
+        self.pnl_rate = pnl_rate
+        self.pos_value_rate = pos_value_rate
+        self.current_pos_rate = current_pos_rate
+
+        addition_reward = abs_pos_value_rate if 0.3 > abs_pos_value_rate >= 0.001 else 0
+        action_reward = -abs(action) if action > 0.2 or action < -0.2 else 0.2 - abs(action)
+        pos_hold_reward = -self.pos_hold_penalty * 0.0001
+
+        reward = (
+                + 50. * current_pos_rate
+                - 1. * abs_pos_value_rate
+                + 1. * pnl_rate
+                + 2. * addition_reward
+                + 1. * action_reward
+                + 1. * pos_hold_reward
+        )
+
+        return scaled_sigmoid(reward, -2, 2) - 0.5
